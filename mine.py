4b2984a0d66ae6bac3c8bce47e39beac8f4d4771
zaloznikjan7 <57746808+zaloznikjan7@users.noreply.github.com>
2025-05-19 21:15:16 +0200

    Update mine.py

--- mine.py
+++ mine.py
@@ -13,11 +13,12 @@ print(f"Delaying execution by {delay} minutes...")
 time.sleep(delay * 60)
 
 token = os.getenv("Auth_bearer")
-
-url = "https://socialchain.app/api/proof_of_presences"
+url = os.getenv("API_URL")
+ref_url = os.getenv("REFERAL_URL")
+host = os.getenv("HOST_NAME")
 
 headers = {
-    "Host": "socialchain.app",
+    "Host": host,
     "w": "2",
     "Accept": "application/json, text/plain, */*",
     "Authorization": f"Bearer {token}",
@@ -25,9 +26,9 @@ headers = {
     "Accept-Language": "en-GB,en;q=0.9",
     "Sec-Fetch-Mode": "cors",
     "Content-Type": "application/json",
-    "Origin": "https://app-cdn.minepi.com",
+    "Origin": ref_url,
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
-    "Referer": "https://app-cdn.minepi.com/",
+    "Referer": ref_url + "/",
     "Connection": "keep-alive",
     "Sec-Fetch-Dest": "empty"
 }
